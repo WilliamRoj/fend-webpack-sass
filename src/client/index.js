d8f5c448cdc226ddbb3b814ffd61598786960f17
William Rojas <william.ur@live.com>
2021-02-10 19:13:17 -0700

    updated code in all files in order to have express run correctly

--- src/client/index.js
+++ src/client/index.js
@@ -3,11 +3,15 @@ import { handleSubmit } from './js/formHandler'
 
 import './styles/resets.scss'
 import './styles/base.scss'
-import './styles/footer.scss'
 import './styles/form.scss'
+import './styles/footer.scss'
 import './styles/header.scss'
 
 console.log(checkForName);
 
 alert("I EXIST")
-console.log("CHANGE!!");
+
+export {
+    checkForName,
+    handleSubmit
+}
